ab52abd16d970b5b486b79e0f944bbaede3aee3e
BatelCo <batel.cohen100@gmail.com>
2022-11-13 19:04:43 +0200

    add users

---
+++ src/components/Users.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react"
+import User from "./User"
+
+class Users extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {}
+	}
+
+	selectUser = (userName) => {
+		this.props.selectUser(userName)
+	}
+
+	getUserElement = (userDetails, key) => (
+		<User key={key} profile={userDetails} selectUser={this.selectUser} />
+	)
+
+	render() {
+		let users = this.props.users
+		return (
+			<div className="selection-container">
+				<h3>WHO'S WATCHING?</h3>
+				<div className="user-invetory">
+					{users.map((u, v) => this.getUserElement(u, v))}
+				</div>
+			</div>
+		)
+	}
+}
+
+export default Users
