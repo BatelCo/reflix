c290d87bce455ff20ba4ae7631c0bcdc9dd3f057
BatelCo <batel.cohen100@gmail.com>
2022-11-14 08:59:17 +0200

    add search bar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default SearchBar;
